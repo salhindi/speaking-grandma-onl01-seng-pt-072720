f3e777d1fcfa90a4e6788ee3c147e87af0dae13c
Sara A <calisaraalhindi@gmail.com>
2020-07-31 20:48:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,14 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
+    elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP SONNY!"
-    elsif phrase == phrase.downcase
+    elsif phrase == "Hi!"
     return "HUH?! SPEAK UP SONNY!"
-    elsif phrase == phrase.upcase
-    return "NO NOT SINCE 1938!"
-     elsif phrase == phrase.upcase
-    return "NO NOT SINCE 1938!"
+    elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+     elsif phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
