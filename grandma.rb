fe48f9b59bc98f2c46077d8b5cf1ed48aa8829f4
Sara A <calisaraalhindi@gmail.com>
2020-07-31 19:40:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = .downcase
-puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase = .upcase
-puts " NO, NOT SINCE 1938!"
+  puts "HUH?! SPEAK UP, SONNY!"
+  else phrase = .upcase
+  puts " NO, NOT SINCE 1938!"
 end 
 
 # Write a speak_to_grandma method.
