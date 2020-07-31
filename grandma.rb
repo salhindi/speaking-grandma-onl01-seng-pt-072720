df22841b92a213f542e625c4ee7561d00b948d85
Sara A <calisaraalhindi@gmail.com>
2020-07-31 19:47:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,4 @@
-def speak_to_grandma
-  if speak_to_grandma = "I LOVE YOU GRANDMA!"
+if speak_to_grandma = "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak_to_grandma.downcase 
   puts "HUH?! SPEAK UP, SONNY!"
