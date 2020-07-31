fbbff6eef4fa2e28ca86b6b555a9d4c6b1dad108
Sara A <calisaraalhindi@gmail.com>
2020-07-31 20:44:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,8 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP SONNY!"
     elsif phrase == phrase.upcase
     puts "NO NOT SINCE 1938!"
-    
+     elsif phrase == phrase.upcase
+    puts "NO NOT SINCE 1938!"
   end
 end
 
