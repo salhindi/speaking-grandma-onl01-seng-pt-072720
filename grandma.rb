f32e6f22d07d0349114908a4a8c4c654bef2cca5
Sara A <calisaraalhindi@gmail.com>
2020-07-31 19:58:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma()
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
   if phrase == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "I LOVE YOU GRANDMA".downcase
