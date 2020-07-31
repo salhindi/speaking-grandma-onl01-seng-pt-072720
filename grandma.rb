c11c17ca00fbd084ca6cfd57bb156b3929f0bb1f
Sara A <calisaraalhindi@gmail.com>
2020-07-31 20:44:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP SONNY!"
+    elsif phrase == phrase.downcase
+    puts "HUH?! SPEAK UP SONNY!"
     elsif phrase == phrase.upcase
     puts "NO NOT SINCE 1938!"
      elsif phrase == phrase.upcase
